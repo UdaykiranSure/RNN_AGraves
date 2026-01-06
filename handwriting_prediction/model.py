79ce8eaff94c0c5710a0fe77f9408d9e95e12a54
UdayKiran <sureudaykiran1@gmail.com>
2026-01-06 11:44:25 +0530

    added hidden state init method and in forward method

--- handwriting_prediction/model.py
+++ handwriting_prediction/model.py
@@ -14,6 +14,11 @@ class model(nn.Module):
         output:
             self.out: mixture model parameter (B * L * out_size)
         """
+        self.in_size = in_size
+        self.h_size = h_size
+        self.out_size = out_size
+        self.num_layers = num_layers
+
         super(model,self).__init__()
         self.rnn_layer = nn.LSTM(in_size, h_size,num_layers,batch_first=True) 
 
@@ -29,17 +34,27 @@ class model(nn.Module):
         # if self.output_layer.bias is not None:
         #     nn.init.zeros_(self.output_layer.bias)
 
-    def forward(self, inputs,lengths):
+    def init_hidden(self, batch_size, device = 'cpu'):
+        h0 = torch.zeros((self.num_layers, batch_size, self.h_size)).to(device)
+        c0 = torch.zeros((self.num_layers, batch_size, self.h_size)).to(device)
+        return (h0, c0)
+
+
+    def forward(self, inputs,lengths, hidden = None):
         packed = pack_padded_sequence(inputs,lengths, batch_first=True,enforce_sorted=False)
-        rnn_out, hidden = self.rnn_layer(packed) # (B * L * H), (B * H)
+        rnn_out, hidden = self.rnn_layer(packed, hidden) # (B * L * H), (B * H)
         seq_unpacked, lens_unpacked = pad_packed_sequence(rnn_out,batch_first=True)
+
         if (lens_unpacked != torch.tensor(lengths)).all():
             print('lengths not matched')
         if not torch.isfinite(seq_unpacked).all():
             print('nan in unpacked')
+
         x = self.output_layer(seq_unpacked) #output params order: (e, 1M(W) 2M(mu), 2M(std), 1M(corr)) => (1 + 6M) ;(B * L * (1+6M))
+        
         if not torch.isfinite(x).all():
             print('nan in model output layer') 
+
         M = (x.shape[-1] - 1) // 6
         e =  torch.sigmoid(x[:,:,0:1]) # e = 1/1 + exp(e_hat) => (B * L* 1)
         W =  F.softmax(x[:,:,1:1+M], -1)          
@@ -48,7 +63,9 @@ class model(nn.Module):
         stdx = torch.exp(torch.clamp(x[:, :, 3*M+1: 4*M+1], min= -10.0, max = 10.0)) 
         stdy = torch.exp(torch.clamp(x[:, :, 4*M+1: 5*M+1],  min= -10.0, max = 10.0))
         corr = torch.tanh(x[:, :, 5*M+1: 6*M + 1])
+
         self.out = torch.cat((e,W,mux,muy,stdx,stdy,corr),dim=-1)
-        return self.out 
+
+        return self.out, hidden
 
     
